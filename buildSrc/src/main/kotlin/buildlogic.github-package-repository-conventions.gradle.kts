30dae309ce41c759ef64ca502a7d40d10fc65abd
René Lebherz <me@renelebherz.com>
2024-08-20 11:49:13 +0200

    chore: throw excpetion when GPR data is missing

--- buildSrc/src/main/kotlin/buildlogic.github-package-repository-conventions.gradle.kts
+++ buildSrc/src/main/kotlin/buildlogic.github-package-repository-conventions.gradle.kts
@@ -3,20 +3,20 @@ plugins {
 }
 
 repositories {
-//    maven {
-//        name = "Particle Packages"
-//        url = uri("https://maven.pkg.github.com/bitfist/particle")
-//        credentials {
-//            username = project.findProperty("GPR_USER") as String? ?: System.getenv("GITHUB_ACTOR")
-//            password = project.findProperty("GPR_KEY") as String? ?: System.getenv("GPR_KEY")
-//        }
-//    }
+    maven {
+        name = "Particle Packages"
+        url = uri("https://maven.pkg.github.com/bitfist/particle")
+        credentials {
+            username = (project.findProperty("GPR_USER") as String? ?: System.getenv("GITHUB_ACTOR")) ?: throw IllegalStateException("GitHub Package Registry username missing")
+            password = (project.findProperty("GPR_KEY") as String? ?: System.getenv("GPR_KEY")) ?: throw IllegalStateException("GitHub Package Registry token missing")
+        }
+    }
     maven {
         name = "Particle Spring Boot Starter Packages"
         url = uri("https://maven.pkg.github.com/bitfist/particle-spring-boot-starter")
         credentials {
-            username = project.findProperty("GPR_USER") as String? ?: System.getenv("GITHUB_ACTOR")
-            password = project.findProperty("GPR_KEY") as String? ?: System.getenv("GPR_KEY")
+            username = (project.findProperty("GPR_USER") as String? ?: System.getenv("GITHUB_ACTOR")) ?: throw IllegalStateException("GitHub Package Registry username missing")
+            password = (project.findProperty("GPR_KEY") as String? ?: System.getenv("GPR_KEY")) ?: throw IllegalStateException("GitHub Package Registry token missing")
         }
     }
 }
