9c5b3ef79b6fb52613a46e1a054bba598eebc862
René Lebherz <me@renelebherz.com>
2024-08-20 11:27:25 +0200

    remove Particle repository

--- buildSrc/src/main/kotlin/buildlogic.github-package-repository-conventions.gradle.kts
+++ buildSrc/src/main/kotlin/buildlogic.github-package-repository-conventions.gradle.kts
@@ -3,14 +3,14 @@ plugins {
 }
 
 repositories {
-    maven {
-        name = "Particle Packages"
-        url = uri("https://maven.pkg.github.com/bitfist/particle")
-        credentials {
-            username = project.findProperty("GPR_USER") as String? ?: System.getenv("GITHUB_ACTOR")
-            password = project.findProperty("GPR_KEY") as String? ?: System.getenv("GPR_KEY")
-        }
-    }
+//    maven {
+//        name = "Particle Packages"
+//        url = uri("https://maven.pkg.github.com/bitfist/particle")
+//        credentials {
+//            username = project.findProperty("GPR_USER") as String? ?: System.getenv("GITHUB_ACTOR")
+//            password = project.findProperty("GPR_KEY") as String? ?: System.getenv("GPR_KEY")
+//        }
+//    }
     maven {
         name = "Particle Spring Boot Starter Packages"
         url = uri("https://maven.pkg.github.com/bitfist/particle-spring-boot-starter")
